3ad3b52113c5ffa53d1a5d334dae5f7f23b871a0
LeandroSilvaPaiva <70709526+LeandroSilvaPaiva@users.noreply.github.com>
2020-11-05 10:47:05 -0300

    angular

---
+++ blogPessoal/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { FeedComponent } from './feed/feed.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    PerfilLateralComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
